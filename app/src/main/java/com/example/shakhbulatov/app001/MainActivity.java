9d638b1acbbb32c06dca76e1c3e86c2c93013f48
ruslan-shah <ruslan-shah@mail.ru>
2018-10-18 19:00:33 +0300

    First

---
+++ app/src/main/java/com/example/shakhbulatov/app001/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.shakhbulatov.app001;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.textView);
+
+        ((Button)findViewById(R.id.btnC)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tv.setText("");
+            }
+        });
+
+    }
+    public void onDigitButtonClick(View view)
+    {
+        tv.setText(tv.getText().toString() + ((Button) view).getText().toString());
+    }
+}
+
